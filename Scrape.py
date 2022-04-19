bb61013164e90613da4b1afcd2e65d9e4c5bafee
FarzinRad <Farzinvado@yahoo.com>
2022-04-19 18:20:31 +0430

    split title, links and votes

--- Scrape.py
+++ Scrape.py
@@ -5,12 +5,21 @@ res = requests.get('https://news.ycombinator.com/news')
 Soup = BeautifulSoup(res.text, 'html.parser')
 
 links = Soup.select('.titlelink')
-votes = Soup.select('.score')
-print(votes[0].get('id'))
+subtext = Soup.select('.subtext')
 
 
-def create_custom_hn(links, votes):
-    hn[]
+def create_custom_func(links, subtext):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].get('href', None)
+        votes = subtext[idx].select('.score')
+        if len(votes):
+            points = int(votes[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'links': href, 'votes': points})
+
+    return hn
 
 
-return hn
+print(create_custom_func(links, subtext))
